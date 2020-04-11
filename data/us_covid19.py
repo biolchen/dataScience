fc386dc9a53cba916cf21032777a7da419a5e648
liangchen <biolchen@gmail.com>
2020-04-10 20:52:16 -0700

    new

--- data/us_covid19.py
+++ data/us_covid19.py
@@ -1,7 +1,8 @@
 us_county=pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv',na_values=0)
 df= us_county.dropna(subset=['fips'])
 df=df.astype({'fips': 'int32'})
-df=df[df.date=='2020-04-06']
+df
+df=df[df.date=='2020-04-09']
 df.head()
 df= df[['state','county','fips','cases']]
 unem=pd.read_csv('https://raw.githubusercontent.com/biolchen/dataScience/master/data/unemployment.tsv', sep='\t')
@@ -9,7 +10,7 @@ unem
 df=pd.merge(unem,df,left_on='id',right_on='fips')[['id','cases','county','state']]
 df[df.state=='California']
 df
-df.to_csv('/Users/liangchen/Downloads/dataScience/data/us_counties.csv',index=False, sep="\t")
+df.to_csv('/Volumes/x5/repo/dataScience/data/us_counties.csv',index=False, sep="\t")
 unem.dtypes
 
 pd.read_json('/Users/liangchen/Downloads/dataScience/data/a.json')
