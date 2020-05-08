db6cce323b478538182d82565b99396351bf5b3f
Liang Chen <18598014+biolchen@users.noreply.github.com>
2020-05-08 12:43:46 -0700

    add css

--- snippet/dataWrangling/binarized.py
+++
@@ -1,3 +0,0 @@
-a=np.random.rand(3,2)
-b=np.zeros((3,2))
-b[:]=a>a.mean()
--- snippet/dataWrangling/get_list_from_txt.py
+++
@@ -1,7 +0,0 @@
-def get_list_from_txt(input_list_f):
-    """a function to read txt file line-by-line into a list
-    """
-    f=open(input_list_f, 'r')
-    output_list=[line.rstrip('\n') for line in f]
-    f.close()
-    return output_list
